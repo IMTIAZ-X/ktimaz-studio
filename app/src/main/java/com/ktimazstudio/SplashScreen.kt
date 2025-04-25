5a977993d0c80a5fb42e1af1dbf4ffa442a3e10e
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-04-25 23:10:01 +0600

    Update SplashScreen.kt

--- app/src/main/java/com/ktimazstudio/SplashScreen.kt
+++ app/src/main/java/com/ktimazstudio/SplashScreen.kt
@@ -4,16 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -38,18 +39,18 @@ fun SplashScreenContent() {
     var startLogoAnim by remember { mutableStateOf(false) }
 
     val whiteScale by animateFloatAsState(
-        targetValue = if (startWhiteAnim) 3f else 0f,
-        animationSpec = tween(durationMillis = 1000),
+        targetValue = if (startWhiteAnim) 1f else 0f,
+        animationSpec = tween(1000),
         label = "whiteScale"
     )
 
     val logoScale by animateFloatAsState(
-        targetValue = if (startLogoAnim) 0.35f else 0f,
-        animationSpec = tween(durationMillis = 500),
+        targetValue = if (startLogoAnim) 1f else 0f,
+        animationSpec = tween(500),
         label = "logoScale"
     )
 
-    LaunchedEffect(true) {
+    LaunchedEffect(Unit) {
         delay(100)
         startWhiteAnim = true
         delay(1000)
@@ -69,12 +70,15 @@ fun SplashScreenContent() {
             modifier = Modifier
                 .size(300.dp)
                 .scale(whiteScale)
-                .background(MaterialTheme.colorScheme.surface, shape = MaterialTheme.shapes.extraLarge),
+                .background(
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = RoundedCornerShape(32.dp)
+                ),
             contentAlignment = Alignment.Center
         ) {
             Image(
                 painter = painterResource(id = R.mipmap.ic_launcher),
-                contentDescription = null,
+                contentDescription = "App Logo",
                 modifier = Modifier
                     .size(150.dp)
                     .scale(logoScale)
