87ce04339c32978c33ba9b6cfbc4146477bfe5c2
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-04-25 08:16:47 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,14 +29,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-        getByName("debug") {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
 
     compileOptions {
