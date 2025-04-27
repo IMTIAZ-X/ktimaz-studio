cfb5e67ae67fc504cff116db5d548d75eaabfd61
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-04-27 23:47:11 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,10 +22,10 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(project.property("RELEASE_STORE_FILE") as String)
-            storePassword = project.property("RELEASE_STORE_PASSWORD") as String
-            keyAlias = project.property("RELEASE_KEY_ALIAS") as String
-            keyPassword = project.property("RELEASE_KEY_PASSWORD") as String
+            storeFile = "Ktimazstudio.keystore"
+            storePassword = "ktimazstudio123"
+            keyAlias = "ktimazstudio"
+            keyPassword = "ktimazstudio123"
         }
     }
 
