a3527a9498e48afe10bd311b397a1ac4d556a46e
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-05-01 08:37:10 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,12 +43,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 
@@ -57,7 +57,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = "1.6.10"
     }
 
     packaging {
