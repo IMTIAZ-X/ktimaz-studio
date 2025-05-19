73fccf85e25dff630512e27eabc28960250e2d5a
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-05-19 08:55:27 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
+def gitCommitHash = "git rev-parse HEAD".execute().text.trim()
+def shortCommitHash = "git rev-parse --short HEAD".execute().text.trim()
+
 android {
     namespace = "com.ktimazstudio"
     compileSdk = 35
@@ -12,100 +15,75 @@ android {
         minSdk = 25
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0 - im${shortCommitHash}"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
+        buildConfigField "String", "VERSION_NAME_WITHOUT_FLAVOR", "\"v7.0.0-SNAPSHOT-${shortCommitHash}\""
     }
 
     signingConfigs {
-        create("release") {  
-            storeFile = file(project.property("RELEASE_STORE_FILE") as String)
-            storePassword = project.property("RELEASE_STORE_PASSWORD") as String
-            keyAlias = project.property("RELEASE_KEY_ALIAS") as String
-            keyPassword = project.property("RELEASE_KEY_PASSWORD") as String
-            enableV1Signing = true
-            enableV2Signing = true
-            enableV3Signing = true
-            enableV4Signing = true
+        release {
+            storeFile file(project.property("RELEASE_STORE_FILE"))
+            storePassword project.property("RELEASE_STORE_PASSWORD")
+            keyAlias project.property("RELEASE_KEY_ALIAS")
+            keyPassword project.property("RELEASE_KEY_PASSWORD")
+            enableV1Signing true
+            enableV2Signing true
+            enableV3Signing true
+            enableV4Signing true
         }
     }
 
     buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            signingConfig = signingConfigs.getByName("release")
+        release {
+            minifyEnabled true
+            shrinkResources true
+            proguardFiles getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            signingConfig signingConfigs.release
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility JavaVersion.VERSION_21
+        targetCompatibility JavaVersion.VERSION_21
     }
 
     kotlin {
-    jvmToolchain(21)
-}
-
+        jvmToolchain(21)
+    }
 
     buildFeatures {
-        compose = true
+        compose true
     }
 
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.15"
     }
 
-    packaging {
-    resources {
-        excludes += setOf(
-            "kotlin/**",
-            "kotlin-tooling-metadata.json",
-            "assets/dexopt/**",
-            "assets/dexopt/baseline.prof",
-            "assets/dexopt/baseline.profm",
-            "META-INF/LICENSE",
-            "META-INF/DEPENDENCIES",
-            "META-INF/*.kotlin_module",
-            "**/DebugProbesKt.bin",
-            "okhttp3/internal/publicsuffix/NOTICE",
-            "okhttp3/**",
-            "/META-INF/{AL2.0,LGPL2.1}"
-        )
-    }
-}
-
-    applicationVariants.all {
-        outputs.all {
-            (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName =
-                "ktimazstudio_release_v${defaultConfig.versionName}.apk"
+    packagingOptions {
+        resources {
+            excludes += [
+                "kotlin/**",
+                "kotlin-tooling-metadata.json",
+                "assets/dexopt/**",
+                "assets/dexopt/baseline.prof",
+                "assets/dexopt/baseline.profm",
+                "META-INF/LICENSE",
+                "META-INF/DEPENDENCIES",
+                "META-INF/*.kotlin_module",
+                "**/DebugProbesKt.bin",
+                "okhttp3/internal/publicsuffix/NOTICE",
+                "okhttp3/**",
+                "META-INF/AL2.0",
+                "META-INF/LGPL2.1"
+            ]
         }
     }
-}
 
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-
-    implementation("androidx.compose.material:material-icons-core:1.7.8") // Or your Compose version
-    implementation("androidx.compose.material:material-icons-extended:1.7.8") // Or your Compose version
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    applicationVariants.all { variant ->
+        variant.outputs.all { output ->
+            def apkName = "ktimazstudio_${variant.name}_v${defaultConfig.versionName}.apk"
+            output.outputFileName = apkName
+        }
+    }
 }
