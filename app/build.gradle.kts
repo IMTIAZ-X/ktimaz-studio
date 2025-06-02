4dad76bb7eda77052fe13b7b4aec1d01ee4b4f43
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-06-02 10:56:11 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,12 +132,6 @@ dependencies {
     implementation("androidx.compose.animation:animation")
     implementation("androidx.compose.animation:animation-core")
 
-
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.14.0-alpha01")
-    implementation("com.airbnb.android:lottie:6.5.2")
-    implementation("com.android.volley:volley:1.2.1")
-
     // Optional
    // implementation("androidx.navigation:navigation-compose:2.7.7")
    // implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
