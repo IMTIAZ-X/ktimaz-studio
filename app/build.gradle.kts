f70e9e66e181564c107ce7f3fba66d74bf3f424b
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-07-09 08:40:52 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ android {
     // THIS IS THE KEY CHANGE FOR UNCOMPRESSED DEX FILES IN GRADLE
     // এটি গ্র্যাডলে আনকম্প্রেসড ডিইএক্স ফাইলের জন্য মূল পরিবর্তন
     aaptOptions {
-        noCompress("dex") // Explicitly tells AAPT not to compress .dex files
+       // noCompress("dex") // Explicitly tells AAPT not to compress .dex files
     }
 
     // Keep this as per Android Developers documentation for useEmbeddedDex
