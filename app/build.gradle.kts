dba914c0dbf2578d0b65f8011d411ca2655559e2
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-08-03 20:42:14 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,6 +130,9 @@ dependencies {
     implementation("androidx.compose.animation:animation-core")
     implementation("androidx.navigation:navigation-compose:2.9.0")
 
+	implementation "androidx.activity:activity-compose:1.9.3"
+	implementation "androidx.compose.foundation:foundation:1.7.4"
+
     // Optional
     // implementation("androidx.navigation:navigation-compose:2.7.7")
     // implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
