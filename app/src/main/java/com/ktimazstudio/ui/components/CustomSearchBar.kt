ba150569545897e39259d6e09b7814232a5f3abf
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-08-05 23:19:33 +0600

    Update CustomSearchBar.kt

--- app/src/main/java/com/ktimazstudio/ui/components/CustomSearchBar.kt
+++ app/src/main/java/com/ktimazstudio/ui/components/CustomSearchBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -27,10 +28,10 @@ fun CustomSearchBar(
         onValueChange = onQueryChange,
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(28.dp),
-        placeholder = { Text(stringResource(R.string.search_placeholder)) },
+        placeholder = { Text("Search...") }, // Replaced with hardcoded string
         singleLine = true,
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-        leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "Search Icon") },
+        leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search Icon") },
         trailingIcon = {
             if (query.isNotEmpty()) {
                 IconButton(onClick = {
