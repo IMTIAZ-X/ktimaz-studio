43f33ef8bcb6d92e9a09447ee2e218c2f43fab68
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-08-06 11:34:55 +0600

    Update CustomSearchBar.kt

--- app/src/main/java/com/ktimazstudio/ui/components/CustomSearchBar.kt
+++ app/src/main/java/com/ktimazstudio/ui/components/CustomSearchBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
