96119595a21a8e145a56ac29890d9121000ccadf
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-10-24 19:46:02 +0600

    Add files via upload

--- app/src/main/java/com/ktimazstudio/ui/components/NavigationRail.kt
+++ app/src/main/java/com/ktimazstudio/ui/components/NavigationRail.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
 import androidx.compose.foundation.shape.RoundedCornerShape
 import com.ktimazstudio.enums.Screen
 import com.ktimazstudio.managers.SoundEffectManager
