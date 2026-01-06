a63d7abf60f905cc3054496380b73ad73caac7be
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2026-01-06 13:12:10 +0600

    Add files via upload

--- app/src/main/java/com/ktimazstudio/agent/ui/settings/ApiManagementSettings.kt
+++ app/src/main/java/com/ktimazstudio/agent/ui/settings/ApiManagementSettings.kt
@@ -1,6 +1,7 @@
 package com.ktimazstudio.agent.ui.settings
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,7 +10,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
-import androidx.compose.material3.BorderStroke
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
