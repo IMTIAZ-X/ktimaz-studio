47d3143a4716cfbd4412ca74b5300a0fa213216c
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2026-01-17 20:48:43 +0600

    Update ApiManagementSettings.kt

--- app/src/main/java/com/ktimazstudio/agent/ui/settings/ApiManagementSettings.kt
+++ app/src/main/java/com/ktimazstudio/agent/ui/settings/ApiManagementSettings.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.text.input.VisualTransformation
 import com.ktimazstudio.agent.data.*
 import com.ktimazstudio.agent.viewmodel.AgentViewModel
 
